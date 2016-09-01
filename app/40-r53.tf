f018b73d8406d557a380ff345bad2390e174e9d0
Craig Watson <craig@bashton.com>
2016-09-01 09:46:28 +0100

    Correcting syntax

--- app/40-r53.tf
+++ app/40-r53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "dns" {
   count   = "${var.create_r53 * (var.create_ha + 1)}"
   zone_id = "${var.r53_zone}"
-  name    = "${replace(var.app_stack,'_','')}${replace(var.app_name,'_',''}-${format("%02d",count.index+1)}.${var.domain}"
+  name    = "${replace(var.app_stack,"_","")}${replace(var.app_name,"_","")}-${format("%02d",count.index+1)}.${var.domain}"
   type    = "A"
   ttl     = "300"
   records = ["${element(aws_instance.instance.*.private_ip,count.index)}"]
